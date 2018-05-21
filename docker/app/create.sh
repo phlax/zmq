709131dce51ec6b615591e492384c813c2ad87a2
Ryan Northey <ryan@synca.io>
2018-05-21 10:13:53 +0100

    Rename create dir to app

---
+++ docker/app/create.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+apt-get update -qq
+apt-get install \
+   -y \
+   -qq \
+   --no-install-recommends \
+   gosu
+
+cp -a /tmp/app/entrypoint.sh /usr/local/bin/entrypoint.sh
+cp -a /tmp/app/run-zmq /usr/local/bin/run-zmq
