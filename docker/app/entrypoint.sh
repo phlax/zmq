ad4d055bf9da02d5487c6e3d1f67dabd37c6d872
Ryan Northey <ryan@synca.io>
2018-05-24 23:37:45 +0100

    Add internal sockets dir and ensure perms

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -2,6 +2,10 @@
 
 export HOME=/home/$APP_USERNAME
 
+mkdir -p /app/sockets
+mkdir -p /sockets
+
 chown $APP_USERNAME:$APP_USERNAME /app/sockets
+chown $APP_USERNAME:$APP_USERNAME /sockets
 
 exec gosu $APP_USERNAME "$@"
